6713b3cfc39d65a7730716d98c411207c5126128
Jeet <jeet@Jeets-MacBook-Pro.local>
2021-10-24 12:03:51 +0530

    input output example - counter app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  counterProgress : number = 0;
+  totalCountDown : number = 15;
   constructor() { }
 
+  decreaseCoutner(event) {
+    this.counterProgress = (this.totalCountDown - event) / this.totalCountDown * 100;
+  }
+
+  completeCounter() {
+    console.log('Countdown has finished')
+  }
 }
--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, OnDestroy, OnChanges , Output } from '@angular/core';
 
 @Component({
   selector: 'app-countdown',
   templateUrl: './countdown.component.html',
   styleUrls: ['./countdown.component.scss']
 })
-export class CountdownComponent{
-
+export class CountdownComponent implements OnInit, OnDestroy, OnChanges{
+  @Input() init : number = null;
+  @Output() OnDecrease = new EventEmitter<number>();
+  @Output() OnComplete = new EventEmitter<void>();
+  private timerRef: any = null;
+  public counter : number = 0;
   constructor() { }
+
+  ngOnInit() :void{
+    this.startCountDown();
+  }
+
+  ngOnChanges(changes): void {
+    console.log('New Changes', changes.init.currentValue);
+    this.startCountDown();
+  }
+
+  startCountDown() {
+    if(this.init && this.init > 0) {
+      this.clearTimeout();
+      this.counter = this.init;
+      this.doCountDown();
+    }
+  }
+
+  doCountDown() {
+    this.timerRef = setTimeout(()=> {
+      this.counter = this.counter - 1;
+      this.processCount();
+    }, 1000)
+  }
+
+  private clearTimeout() {
+    if(this.timerRef) {
+      clearTimeout(this.timerRef);
+      this.timerRef = null;
+    }
+  }
+
+  processCount() {
+    this.OnDecrease.emit(this.counter);
+    console.log('Counter is', this.counter);
+    if(this.counter == 0) {
+      this.OnComplete.emit();
+      console.log("-- End Counter--");
+    } else {
+      this.doCountDown();
+    }
+  }
+
+  ngOnDestroy(): void{
+    this.clearTimeout();
+  }
 
 }
